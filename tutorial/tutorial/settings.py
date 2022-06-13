716ce06c6a77f46571d4a8355db555e9e85bd1a3
wac <guillaume.thiebaud@epitech.eu>
2022-06-13 11:16:49 +0200

    setup db

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db.mysql',
+        'OPTIONS': {
+            'database' : 'djangostart',
+            'user' : '',
+            'password' : '',
+        },
     }
 }
 
